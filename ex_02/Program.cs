7f80c5e7c0a4ce87445235070e25369526ddc3e2
VADIM <sedashvadik@yandex.ru>
2022-02-07 19:07:50 +0300

    Решена задача №2

---
+++ ex_02/Program.cs
@@ -0,0 +1,14 @@
+﻿// №2. Даны 2 числа. Показать большее и меньшее число
+
+Console.Write("Введите первое число: ");
+string firstNumber = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string secondNumber = Console.ReadLine();
+
+int first = int.Parse(firstNumber);
+int second = int.Parse(secondNumber);
+
+if(first > second)
+Console.Write($"Наибольшее число -> {first}. Наименьшее число -> {second}");
+else
+Console.Write($"Наибольшее число -> {second}. Наименьшее число -> {first}");
