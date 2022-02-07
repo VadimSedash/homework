e2408083353d19f72d47cf087fc48847d2b35431
VADIM <sedashvadik@yandex.ru>
2022-02-07 19:19:35 +0300

    Задача №6 решена

---
+++ ex_06/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 6. Выяснить является ли число чётным.
+int number = new Random().Next(1,100);
+Console.WriteLine("Сгенерировано число:" + number);
+
+//делим с остатком на 2
+int ostatok = number % 2;
+if(ostatok == 0)
+Console.WriteLine("Число является чётным");
+else
+Console.WriteLine("Число является нечётным");
