b04170094969801feb0be79a60f6d4f675026684
VADIM <sedashvadik@yandex.ru>
2022-02-07 19:23:18 +0300

    Задача №8 решена

---
+++ ex_08/Program.cs
@@ -0,0 +1,11 @@
+﻿// № 8. Показать четные числа от 1 до N
+Console.Write("Введите число (до него будут показаны все четные числа) ->  ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+int chetnoe = 2;
+while(chetnoe <= number)
+{
+   Console.WriteLine(chetnoe);
+   chetnoe = chetnoe + 2;
+}
